590ccd5929678a2699bdd2c1cc42ea9e92efb722
雷春丰 <leichunfeng@ysstech.com>
2019-05-20 14:21:52 +0800

    修正错误字段和名称

--- src/main/resources/init_test.sql
+++ src/main/resources/init_test.sql
@@ -3,8 +3,10 @@ use test;
 CREATE TABLE IF NOT EXISTS `department`(
    `id` VARCHAR(100) NOT NULL,
    `name` VARCHAR(100) NOT NULL,
-   `employer_id` int(11) NOT NULL,
+   `employer_id` VARCHAR(100) NOT NULL,
    `create_date` DATE,
    `modify_date` DATE,
    PRIMARY KEY ( `id` )
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+alter table department modify employer_id VARCHAR(100) NOT NULL;
