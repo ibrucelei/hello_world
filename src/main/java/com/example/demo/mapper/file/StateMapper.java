5606dc84e5fea633584c48c9b8fcab945fb87c52
雷春丰 <leichunfeng@ysstech.com>
2019-05-21 18:07:10 +0800

    设置mybatis二级缓存。

--- src/main/java/com/example/demo/mapper/file/StateMapper.java
+++ src/main/java/com/example/demo/mapper/file/StateMapper.java
@@ -2,26 +2,24 @@ package com.example.demo.mapper.file;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Delete;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Result;
 import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.annotations.Update;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-
 import com.example.demo.domain.StateEntity;
 @Mapper
+@CacheNamespace
 public interface StateMapper {
 	@Select("SELECT * FROM state")
 	@Results({
 		//@Result(property = "userSex",  column = "user_sex", javaType = UserSexEnum.class),
 		//@Result(property = "nickName", column = "nick_name")
 	})
+	
 	List<StateEntity> getAll();
 	
 	@Select("SELECT * FROM state WHERE id = #{id}")
