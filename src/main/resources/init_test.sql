fa7facfd384fcc53d3ab7c57b353ef6c8ab1592b
雷春丰 <leichunfeng@ysstech.com>
2019-05-22 16:29:43 +0800

    role_name不需要以role开头。

--- src/main/resources/init_test.sql
+++ src/main/resources/init_test.sql
@@ -38,6 +38,6 @@ CREATE TABLE IF NOT EXISTS `user_role`(
 )ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into user(id,user_name,password) values('001','root','password');
-insert into role(id,role_name) values('001','role_root');
+insert into role(id,role_name) values('001','admin');
 insert into user_role(id,user_id,role_id) values('001','001','001');
 SELECT r.* from user_role ur,role r where ur.user_id=r.id and ur.user_id=''
